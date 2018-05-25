4a635325f158ccb76d7835aa163207199756f636
fr3006 <fr3006@gmail.com>
2018-05-24 22:31:52 -0500

    primero modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MapaComponent } from './mapa/mapa.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  animal: string;
+  constructor(public dialog: MatDialog) {}
+  openDialog(): void {
+
+   const dialogRef = this.dialog.open(MapaComponent, {
+      data: {
+        animal: 'panda'
+      }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('ya');
+      this.animal = result;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MapaComponent } from './mapa/mapa.component';
 
 @NgModule({
+  entryComponents: [
+    MapaComponent
+  ],
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
